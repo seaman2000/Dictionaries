da6badf70e75b7ae7dc5bed4082ca2b066d44da7
Georgi Angelov <gtangelov76@gmail.com>
2026-02-22 22:23:17 +0200

    Update car_models.py

--- Dictionaries_pratice_with_ChatGPT/car_models.py
+++ Dictionaries_pratice_with_ChatGPT/car_models.py
@@ -4,4 +4,11 @@ cars = {
     "Honda": 201,
     "Mercedes": 270
 }
+max_hp = 0
+car_model = ""
+for key, value in cars.items():
+    if value > max_hp:
+        max_hp = value
+        car_model = key
 
+print(f"The fastest car between is {car_model} with {max_hp}")
