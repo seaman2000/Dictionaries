d25a86d14f2be22e2a4ed784a566a4d80a4edb6d
Georgi Angelov <gtangelov76@gmail.com>
2026-03-04 20:29:10 +0200

    Update odd_occurencies.py

--- Dictionaries_Lab/odd_occurencies.py
+++ Dictionaries_Lab/odd_occurencies.py
@@ -1,1 +1,12 @@
 words_in_line = input().split()
+odd_dict = {}
+
+for word in words_in_line:
+    lower_word = word.lower()
+    if lower_word not in odd_dict:
+        odd_dict[lower_word] = 0
+    odd_dict[lower_word] += 1
+
+for key, value in odd_dict.items():
+    if value % 2 == 1:
+        print(key, end=' ')
