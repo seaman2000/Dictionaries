5a82fb2d871320102d025d5d00f04268f98175c5
Georgi Angelov <gtangelov76@gmail.com>
2026-03-06 11:54:14 +0200

    Update legendary_farming.py

---
+++ Dictionaries_Exercises/legendary_farming.py
@@ -0,0 +1,28 @@
+legendary_resources = {
+    "Shards": 0,
+    "Fragments": 0,
+    "Motes": 0
+}
+legendary_is_obtained = False
+while True:
+    quantity = int(input())
+    material = input()
+    legendary_resources[material] = legendary_resources.get(material, 0)
+    legendary_resources[material] += quantity
+
+    if legendary_resources["Shards"] >= 250:
+        print("Shadowmourne obtained!")
+        legendary_resources["Shards"] -= 250
+        legendary_is_obtained = True
+    elif legendary_resources["Fragments"] >= 250:
+        print("Valanyr obtained!")
+        legendary_resources["Fragments"] -= 250
+        legendary_is_obtained = True
+    elif legendary_resources["Motes"] >= 250:
+        print("Dragonwrath obtained!")
+        legendary_resources["Motes"] -= 250
+        legendary_is_obtained = True
+        
+    if legendary_is_obtained:
+        break
+
