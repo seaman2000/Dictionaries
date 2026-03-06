d4595a6b41b96865d42bd7a67477a25741c49387
Georgi Angelov <gtangelov76@gmail.com>
2026-03-06 18:14:29 +0200

    Update softuni_exam_results.py

--- Dictionaries_Exercises/softuni_exam_results.py
+++ Dictionaries_Exercises/softuni_exam_results.py
@@ -6,23 +6,25 @@ while True:
 
     if submission == "exam finished":
         break
+    parts = submission.split("-")
+    if len(parts) == 2:
+        name, command = parts
+        if command == "banned":
+            del exam_results[name]
+    else:
+        name, language, points = parts
+        points = int(points)
+        exam_results.setdefault(name, 0)
+        submissions[language] = submissions.get(language, 0) + 1
 
-    name, language, points = submission.split("-")
-    if points == "banned":
-        del exam_results[name]
-        continue
-
-    points = int(points)
-    exam_results.setdefault(name, {})
-    submissions[language] = submissions.get(language, 0) + 1
-
-    if points > exam_results[name].get(language, 0):
-        exam_results[name][language] = points
+        if points > exam_results[name].get(language, 0):
+            exam_results[name] = points
 
 print("Results:")
 for username, points in exam_results.items():
     for language, value in points.items():
         print(f"{username} | {value}")
 
+print("Submissions:")
 for language, count in submissions.items():
     print(f"{language} - {count}")
