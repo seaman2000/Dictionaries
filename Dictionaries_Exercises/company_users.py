9959a8786971618fe976164034f9835fb0dc3217
Georgi Angelov <gtangelov76@gmail.com>
2026-03-06 16:33:32 +0200

    Update company_users.py

--- Dictionaries_Exercises/company_users.py
+++ Dictionaries_Exercises/company_users.py
@@ -2,6 +2,15 @@ company_users = {}
 
 while True:
     command = input()
-    if command == "end":
+    if command == "End":
         break
     company_name, user_id = command.split(" -> ")
+    company_users.setdefault(company_name, [])
+
+    if user_id not in company_users[company_name]:
+        company_users[company_name].append(user_id)
+
+for company, user_list in company_users.items():
+    print(company)
+    for user in user_list:
+        print(f"-- {user}")
