9d93ad40ac8f25e765490e15df0965fb2654fa27
Georgi Angelov <gtangelov76@gmail.com>
2026-03-06 17:13:07 +0200

    Update force_book.py

--- Dictionaries_Exercises/force_book.py
+++ Dictionaries_Exercises/force_book.py
@@ -1,7 +1,6 @@
 force_book = {}
 
 while True:
-    user_exists = False
     input_line = input()
 
     if input_line == "Lumpawaroo":
@@ -9,23 +8,29 @@ while True:
 
     if "|" in input_line:
         side, user = input_line.split(" | ")
-        force_book.setdefault(side, [])
 
+        user_exists = False
         for users in force_book.values():
             if user in users:
                 user_exists = True
+                break
 
         if not user_exists:
-            force_book[side].append(user)
+            force_book.setdefault(side, []).append(user)
 
     elif "->" in input_line:
         user, side = input_line.split(" -> ")
+
         for users in force_book.values():
             if user in users:
                 users.remove(user)
                 break
+
         force_book.setdefault(side, []).append(user)
-        
         print(f"{user} joins the {side} side!")
 
-print(force_book)
+for force, users in force_book.items():
+    if users:
+        print(f"Side: {force}, Members: {len(users)}")
+        for user in users:
+            print(f"! {user}")
